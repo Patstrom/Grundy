e21d9cb250f6cc5a85b99da40d3dc4f37330d553
Patrik Karlström <jp.karlstrom@gmail.com>
2016-07-10 15:44:03 +0200

    Improve mex algorithm

--- jockeskex/jockeskex.cpp
+++ jockeskex/jockeskex.cpp
@@ -1,32 +1,45 @@
 #include "stdafx.h"
+#include <chrono>
 #include <vector>
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <set>
 
-int mex(const std::vector<int>& lst);
+int mex(std::vector<int>& lst);
 int gv_next(const std::vector<int>& lst);
 void gv_listing(int n, std::vector<int>& lst);
+
+using namespace std::chrono;
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::vector<int> lst = { 0, 1, 0, 2, 2, 4, 1, 1 };
 
-	gv_listing(2000, lst);
-	std::for_each(lst.begin(), lst.end(), [](int i) {std::cout << i << ' '; });
+	auto t1 = high_resolution_clock::now();
+	gv_listing(5000, lst);
+	auto t2 = high_resolution_clock::now();
+
+	auto duration = duration_cast<seconds> (t2 - t1).count();
+	std::cout << duration << std::endl;
+
+	//std::for_each(lst.begin(), lst.end(), [](int i) {std::cout << i << ' '; });
 
 	std::getchar();
 }
 
-int mex(const std::vector<int>& lst) {
-	//TODO: Sort first? Is it worth it?
-	int i = 0;
-	auto index = std::find(lst.begin(), lst.end(), i);
-	while (index != lst.end()) {
-		i++;
-		index = std::find(lst.begin(), lst.end(), i);
+int mex(std::vector<int>& lst) {
+	std::sort(lst.begin(), lst.end());
+
+	if (lst[0] != 0) return 0;
+	int size = lst.size();
+	for (int i = 0; i < size - 1; i++) {
+		int diff = lst[i + 1] - lst[i];
+		if (diff > 1)
+			return lst[i]+1;
 	}
-	return i;
+	return *lst.rbegin() + 1;
 }
 
 int gv_next(const std::vector<int>& lst) {
